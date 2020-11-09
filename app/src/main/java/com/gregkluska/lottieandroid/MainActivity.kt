c08f9807e89aa7674537dadf8347732757ce8534
Greg Kluska <kluskagreg@gmail.com>
2020-11-09 21:39:32 +0000

    Initial commit

---
+++ app/src/main/java/com/gregkluska/lottieandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gregkluska.lottieandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
