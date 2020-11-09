e0c510106570cbaa18bd2498d9f43dbe4afeb399
Greg Kluska <kluskagreg@gmail.com>
2020-11-09 22:27:18 +0000

    Add simple animation

--- app/src/main/java/com/gregkluska/lottieandroid/MainActivity.kt
+++ app/src/main/java/com/gregkluska/lottieandroid/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.gregkluska.lottieandroid
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.Animation
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animationView = findViewById<LottieAnimationView>(R.id.LottieView)
+        animationView.setAnimation("drinks.json")
+        animationView.repeatCount = Animation.INFINITE
+        animationView.repeatMode = LottieDrawable.RESTART
+
+        animationView.setOnClickListener {
+            animationView.playAnimation()
+        }
     }
+
 }
